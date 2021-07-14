2f761cf52251415832fb7464631109fb44551bd2
Galahadriel <gabriel_rivera9@hotmail.com>
2021-07-14 03:54:52 -0600

    Staged

---
+++ hustler.py
@@ -0,0 +1,25 @@
+##This is an App for The Daily Hustler 
+
+# We need to create an application to manage club entries 
+
+
+# The Top Shelf
+
+# The Club has access to Event Tickets 
+# The Club has access to Stoner Merc
+# The Club has access to Chill Spots
+
+# The Members have an account
+# The Members have coin
+# The Members have different plans 
+# The Members have referals 
+# The Members have free monthly packages from stores depending on plans
+# The Members have access to Treasure Hunt 
+# The Members have access to club equipment
+
+# The Members Can Buy Goodies
+# The Members
+
+
+y = 0 
+x = 0 
